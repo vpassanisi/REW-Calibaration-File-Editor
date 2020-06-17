405d392f242346f53b8ca9d41f73fe3d4884c123
vpassanisi <vpassanisi@gmail.com>
2020-06-16 17:20:37 -0700

    added build for deploy

---
+++ build/precache-manifest.53eaee279193f28f9da9abc901ed2b3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf16e9ff1906de1384b6d6af11b63c9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "201971a78dba384a2d54",
+    "url": "/static/css/main.7b1f3862.chunk.css"
+  },
+  {
+    "revision": "2285e1f5a245d0a9b5d7",
+    "url": "/static/js/2.dc93174b.chunk.js"
+  },
+  {
+    "revision": "e72ca98b13d118ca09ad13ced832fbfd",
+    "url": "/static/js/2.dc93174b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "201971a78dba384a2d54",
+    "url": "/static/js/main.016f7088.chunk.js"
+  },
+  {
+    "revision": "21c054a4c9dc895bb2fc",
+    "url": "/static/js/runtime-main.051ab17d.js"
+  }
+]);
